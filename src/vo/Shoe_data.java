a6168b77bcf5daacf1683d2fefe1658232bb70a8
acorn-PC\acorn <mpark0402@gmail.com>
2017-07-10 10:18:11 +0900

    initial commit

---
+++ src/vo/Shoe_data.java
@@ -0,0 +1,13 @@
+package vo;
+
+public class Shoe_data {
+
+	private int no;
+	private String brand;
+	private String pname;
+	private String detail;
+	private String gender;
+	private String color;
+	private String img_add;
+	private String page_add;
+}
